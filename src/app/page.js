9551a6357e9d422feabd705418dd2e15bdb79f02
Hebert Richard <hebertdev82@gmail.com>
2024-02-23 12:02:55 -0300

    feat: construction page

--- src/app/page.js
+++ src/app/page.js
@@ -2,19 +2,13 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <main className="flex min-h-screen flex-col justify-between align-center justify-center text-center text-3xl font-bold mt-28">
-        {/* <Home /> */}
-        <div>
-          <h2 className="align-center justify-center text-center text-3xl font-bold mt-28">
-            Em propagação!
-          </h2>
-          <h4 className="align-center justify-center text-center text-xl">
-            Volte em 48h
-          </h4>
-          {/* <Timer /> */}
-        </div>
-      </main>
-    </main>
+    <figure id="container">
+      <Image
+      src="/bg.jpg"
+      width={1920}
+      height={1280}
+      / >
+      <figcaption>Bolão do Mineiro</figcaption>
+    </figure>
   );
 }
