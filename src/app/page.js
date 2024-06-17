116aaa23cd8950b33fd07a99aa031d6e6b89f8fb
Hebert <hebertdev82@gmail.com>
2024-06-17 14:24:31 -0300

    feat: app structure

--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,35 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <figure id="container">
-      <Image
-      src="/bg.jpg"
-      width={1920}
-      height={1280}
-      / >
-      <figcaption>Bolão do Mineiro</figcaption>
-    </figure>
+    <>
+      <div className="flex flex-col text-center align-center justify-center">
+        <div className="m-8">
+          <div className="p-16 border border-black">Navegação</div>
+        </div>
+
+        <div className="m-8">
+          <div className="p-16 border border-black">Slider</div>
+        </div>
+
+        <div className="m-8">
+          <div className="p-16 border border-black">Apostas disponíveis</div>
+        </div>
+
+        <div className="m-8">
+          <div className="p-16 border border-black">Nossos números</div>
+        </div>
+
+        <div className="m-8">
+          <div className="p-16 border border-black">Como jogar?</div>
+        </div>
+
+        <div className="m-8">
+          <div className="p-16 border border-black">Depoimentos</div>
+        </div>
+
+        <div className="m-8">
+          <div className="p-16 border border-black">Rodapé</div>
+        </div>
+      </div>
+    </>
   );
 }
