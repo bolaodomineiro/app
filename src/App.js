425168f0139e7c99068d16fe75416d8494b65201
Hebert <hebertdev82@gmail.com>
2024-07-10 16:51:35 -0300

    feat: under construction page

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <br />
+        <br />
         <p>Bolão do Mineiro</p>
         <p>Onde a diversão é garantida!</p>
       </header>
