9fd6538f384583b42a2d641dcb74f687641f6a16
manoel <piloukos45@gmail.com>
2024-07-11 11:28:04 -0300

    fix:(branch) corrigir falha.

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
 import "./App.css";
+import Logo from "../src/logotipo.png";
 
 function App() {
   return (
     <>
+      <img src={Logo} className="App-logo" alt="logo" />
       <p>Bolão do Mineiro</p>
       <p>Onde a diversão acontece!</p>
     </>
