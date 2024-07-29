ed148ff761f4590a08c04445adb10928ba0e7839
manoel <piloukos45@gmail.com>
2024-07-29 12:41:57 -0300

    fix: reload dist

--- src/components/Carousel/Slider.jsx
+++ src/components/Carousel/Slider.jsx
@@ -8,7 +8,7 @@ import 'swiper/css';
 import 'swiper/css/pagination';
 import 'swiper/css/navigation';
 import { Pagination, Navigation, Autoplay } from 'swiper/modules';// import required modules
-import Slid01 from  '../../../dist/assets/slide.avif';// import banners
+import Slid01 from  '../../../dist/assets/slide.avif';
 import Card from "../cards/CardBenerfit/Card";
 
 const Slider = () => {
