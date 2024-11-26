4faa356bb4ac277c31107e948544e1a74fdc630c
hrm4k3r <hebertdev82@gmail.com>
2024-11-26 10:25:37 -0300

    feat: add link to admin page

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -2,23 +2,16 @@ import React, { useState, useRef } from 'react';
 import Logo from '../../assets/images/logotipo.png';
 import { HeaderContainer, Search_area } from './HeaderStyles';
 import { Link } from 'react-router-dom';
-
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faMagnifyingGlass, faCartShopping, faBars, faSquareXmark} from '@fortawesome/free-solid-svg-icons';
+import { faMagnifyingGlass, faCartShopping, faBars, faSquareXmark } from '@fortawesome/free-solid-svg-icons';
 import Menu from '../navigation/Menu';
 import Btn from '../button/Btn';
-import {useAuthContext} from "../../context/AuthContext"
-
-
+import { useAuthContext } from "../../context/AuthContext"
 
 const Header = () => {
-
     const [menu, setMenu] = useState(true);
-
     const { Authenticated } = useAuthContext();
-
     const headerRef = useRef(null);
-
     const handleClickOutside = (event) => {
         if (headerRef.current && !headerRef.current.contains(event.target)) {
             setMenu(true);
@@ -31,13 +24,22 @@ const Header = () => {
             <div className='header_top_area'>
                 <img className='logo' src={Logo} alt="logo" />
                 <div className='header_top_area_right'>
-                    
+
                     <Search_area>
-                        <input type="text" placeholder="Buscar aposta"/>
+                        <input type="text" placeholder="Buscar aposta" />
                         <FontAwesomeIcon className='search_icone' icon={faMagnifyingGlass} />
                     </Search_area>
                     <div>
-                        {Authenticated ? (
+
+                        <Link
+                            className='link'
+                            to="https://painel.bolaodomineiro.com.br"
+                        >
+                            Entrar
+                        </Link>
+
+
+                        {/* {Authenticated ? (
                                 <div className='user_area'>
                                     <div> 
                                         Bem vindo,  
@@ -66,36 +68,36 @@ const Header = () => {
                                     </Link>
                                 </>
                             )
-                        }
+                        } */}
                     </div>
-                    
-                    <div className='cart_area' style={{display: Authenticated ? "flex" : "none"}}>
+
+                    <div className='cart_area' style={{ display: Authenticated ? "flex" : "none" }}>
                         <FontAwesomeIcon className='cart_icone' icon={faCartShopping} />
                         <div className='cart_info' >
                             <p>Saldo: R$ 0,00</p>
-                            <Btn text={"Adicionar saldo"}/>
+                            <Btn text={"Adicionar saldo"} />
                         </div>
                     </div>
                 </div>
             </div>
-            <section 
+            <section
                 className='header_footer_area'
             >
                 <Menu toggleMenu={menu} setMenu={setMenu} />
 
-                {   
-                    menu ? <FontAwesomeIcon 
-                        className='menu_icone' 
-                        icon={faBars} 
+                {
+                    menu ? <FontAwesomeIcon
+                        className='menu_icone'
+                        icon={faBars}
                         onClick={() => setMenu(!menu)}
                     /> :
-                    <FontAwesomeIcon 
-                        className='menu_icone' 
-                        icon={faSquareXmark} 
-                        onClick={() => setMenu(!menu)}
-                    />
+                        <FontAwesomeIcon
+                            className='menu_icone'
+                            icon={faSquareXmark}
+                            onClick={() => setMenu(!menu)}
+                        />
                 }
-                
+
             </section>
         </HeaderContainer>
     );
