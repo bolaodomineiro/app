363ebe0a041c357d195806d197abba9499c9f0c7
hrm4k3r <hebertdev82@gmail.com>
2025-01-29 18:10:44 -0300

    fix: authentication fail

--- src/context/AuthContext.jsx
+++ src/context/AuthContext.jsx
@@ -1,59 +1,49 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
-import {  signOut, onAuthStateChanged } from "firebase/auth";
+import { signOut, onAuthStateChanged } from "firebase/auth";
 import { auth } from "../config/firebase.config";
 import { useNavigate } from "react-router-dom";
 
 const AuthContext = createContext();
 
 export function AuthProvider({ children }) {
-
     const navigate = useNavigate();
-    
     const [Authenticated, setAuthenticated] = useState(false);
-
 
     const logout = async () => {
         try {
             await signOut(auth);
             setAuthenticated(false);
+            localStorage.removeItem("token");
             localStorage.clear();
             navigate("/login");
-            console.log("Usuário deslogado.");
         } catch (error) {
-            console.error("Erro ao deslogar:", error.message);
+            console.error(error.message);
         }
     };
 
     useEffect(() => {
-        // Ouvinte para verificar se há um usuário autenticado
         const unsubscribe = onAuthStateChanged(auth, async (user) => {
             if (user) {
                 try {
-                    // Obtém o token atualizado
                     const tokenResult = await user.getIdTokenResult(true);
-                    
-                    // Converte expirationTime para timestamp correto
-                    const expirationTime = new Date(tokenResult.expirationTime).getTime();
-                    const now = Date.now();
+                    localStorage.setItem("token", tokenResult.token);
 
-                    if (expirationTime < now) {
+                    if (tokenResult.expirationTime < Date.now() / 1000) {
                         throw new Error("Token expirado.");
                     }
 
-                    console.log("Token válido. Usuário continua logado.");
-                    localStorage.setItem("token", tokenResult.token);
                     setAuthenticated(true);
                 } catch (error) {
-                    console.error("Token inválido ou expirado:", error.message);
+                    console.error(error.message);
                     logout();
                 }
             } else {
-                console.log("Nenhum usuário autenticado.");
+                alert("Erro. Por favor, tente autenticar novamente.");
                 setAuthenticated(false);
             }
         });
 
-        return () => unsubscribe(); // Remove o listener quando o componente desmontar
+        return () => unsubscribe();
     }, []);
 
     return (
@@ -64,4 +54,3 @@ export function AuthProvider({ children }) {
 }
 
 export const useAuthContext = () => useContext(AuthContext);
-
